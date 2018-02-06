a7908b13892b59ef88f85cf10e02ba40abfecf1a
Caroline <weiqianp9165@gmail.com>
2018-02-05 23:02:35 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,59 @@
+/* Unit 3 code records 
+
+var cardOne = "quenn";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+*/
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+}
+
+var flipCard = function (cardId) {
+	console.log ("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+}
+
+ 
+flipCard(0);
+flipCard(2);
